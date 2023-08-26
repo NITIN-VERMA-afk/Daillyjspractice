438845e6722871db490ffb9cbd27533bc2cd34fc
Nitin Verma <nitinvermanv61506@gmail.com>
2023-08-27 00:10:33 +0530

    errorhandling

---
+++ index.js
@@ -0,0 +1,9 @@
+try {
+  console.log("hello nitin");
+} catch (errorType1) {
+  console.log("there is some error");
+} finally {
+  console.log(
+    "this code will run no matter what happens in the try and catch blocks"
+  );
+}
