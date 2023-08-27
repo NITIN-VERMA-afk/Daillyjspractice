efa35534ee9cdd73d6f36c43554a39742576e982
Nitin Verma <nitinvermanv61506@gmail.com>
2023-08-27 18:03:36 +0530

    spreadand rest

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-try {
-  console.log("hello nitin");
-} catch (errorType1) {
-  console.log("there is some error");
-} finally {
-  console.log(
-    "this code will run no matter what happens in the try and catch blocks"
-  );
-}
+
+// error handling
+// try {
+//   console.log("hello nitin");
+// } catch (errorType1) {
+//   console.log("there is some error");
+// } finally {
+//   console.log(
+//     "this code will run no matter what happens in the try and catch blocks"
+//   );
+// }
+// spread operator with an array
+// let arr1=[23,23,4,56,76]
+// let arr2=[9,76,34,54,21,65]
+
+// const combine=[...arr1,...arr2];
+// console.log(combine);
+// spread with object 
+// let obj1={
+//   a:1,
+//   b:45
+// }
+// let obj2={
+//   c:56,
+//   d:55
+// }
+
+// const combineobj={...obj1,...obj2}
+// console.log(combineobj);
+// rest operator
+
+
+// const sum = (...numbers) => {
+//   let total = 0;
+//   for (const num of numbers) {
+//     total += num;
+//   }
+//   return total;
+// }
+
+// console.log(sum(1, 2, 3, 4, 5, 6, 7));
+
